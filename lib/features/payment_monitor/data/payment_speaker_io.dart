d78917ff9463cf39bdb367bc7ca6d03cc991fc8f
hoanghochoi <hoanghochoi1618@gmail.com>
2026-05-22 21:01:31 +0700

    Use native Windows audio playback for payments

--- lib/features/payment_monitor/data/payment_speaker_io.dart
+++ lib/features/payment_monitor/data/payment_speaker_io.dart
@@ -1,6 +1,8 @@
+import 'dart:ffi';
 import 'dart:io';
+import 'dart:isolate';
 
-import 'package:audioplayers/audioplayers.dart';
+import 'package:ffi/ffi.dart';
 import 'package:flutter/services.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -24,12 +26,12 @@ class PaymentSpeaker {
           'bytes': audioBytes?.length ?? 0,
         },
       );
-      await _playTingTing();
+      final directory = await getTemporaryDirectory();
+      await _playTingTing(directory);
       if (audioBytes == null || audioBytes.isEmpty) {
         throw StateError('Server audio is empty');
       }
 
-      final directory = await getTemporaryDirectory();
       final extension = _audioExtension(audioBytes);
       final file = File(
         '${directory.path}${Platform.pathSeparator}opshub-payment-${DateTime.now().microsecondsSinceEpoch}.$extension',
@@ -37,7 +39,7 @@ class PaymentSpeaker {
       await file.writeAsBytes(audioBytes, flush: true);
       await AppLogger.instance.info(
         _source,
-        'Playing server payment audio',
+        'Playing server payment audio through Windows MCI',
         context: {
           'amount': amount,
           'extension': extension,
@@ -45,14 +47,9 @@ class PaymentSpeaker {
           'path': file.path,
         },
       );
-      final player = AudioPlayer();
       try {
-        await player.play(DeviceFileSource(file.path));
-        await player.onPlayerComplete.first.timeout(
-          const Duration(seconds: 20),
-        );
+        await _playAudioFile(file.path, const Duration(seconds: 20));
       } finally {
-        await player.dispose();
         await file.delete().catchError((_) => file);
       }
     } catch (error, stackTrace) {
@@ -77,8 +74,7 @@ class PaymentSpeaker {
     return 'mp3';
   }
 
-  Future<void> _playTingTing() async {
-    final player = AudioPlayer();
+  Future<void> _playTingTing(Directory directory) async {
     try {
       await AppLogger.instance.info(
         _source,
@@ -86,15 +82,85 @@ class PaymentSpeaker {
         context: {'asset': 'data/ting_ting.mp3'},
       );
       final asset = await rootBundle.load('data/ting_ting.mp3');
+      final file = File(
+        '${directory.path}${Platform.pathSeparator}opshub-ting-ting.mp3',
+      );
+      await file.writeAsBytes(asset.buffer.asUint8List(), flush: true);
       await AppLogger.instance.info(
         _source,
-        'Playing payment sound cue',
-        context: {'bytes': asset.lengthInBytes},
+        'Playing payment sound cue through Windows MCI',
+        context: {'bytes': asset.lengthInBytes, 'path': file.path},
       );
-      await player.play(BytesSource(asset.buffer.asUint8List()));
-      await player.onPlayerComplete.first.timeout(const Duration(seconds: 5));
-    } finally {
-      await player.dispose();
+      await _playAudioFile(file.path, const Duration(seconds: 5));
+    } catch (error, stackTrace) {
+      await AppLogger.instance.warn(
+        _source,
+        'Payment sound cue skipped',
+        context: {
+          'error': error.toString(),
+          'stackTrace': stackTrace.toString(),
+        },
+      );
     }
   }
+
+  Future<void> _playAudioFile(String path, Duration timeout) {
+    return Isolate.run(() => _playAudioFileWithMci(path)).timeout(timeout);
+  }
+}
+
+typedef _MciSendStringNative =
+    Uint32 Function(Pointer<Utf16>, Pointer<Utf16>, Uint32, IntPtr);
+typedef _MciSendStringDart =
+    int Function(Pointer<Utf16>, Pointer<Utf16>, int, int);
+typedef _MciGetErrorStringNative =
+    Int32 Function(Uint32, Pointer<Utf16>, Uint32);
+typedef _MciGetErrorStringDart = int Function(int, Pointer<Utf16>, int);
+
+void _playAudioFileWithMci(String path) {
+  final alias = 'opshub${DateTime.now().microsecondsSinceEpoch}';
+  final type = path.toLowerCase().endsWith('.mp3') ? 'mpegvideo' : 'waveaudio';
+  try {
+    _sendMciCommand('open "${_escapeMciPath(path)}" type $type alias $alias');
+    _sendMciCommand('play $alias wait');
+  } finally {
+    _sendMciCommand('close $alias', throwOnError: false);
+  }
+}
+
+void _sendMciCommand(String command, {bool throwOnError = true}) {
+  final winmm = DynamicLibrary.open('winmm.dll');
+  final mciSendString = winmm
+      .lookupFunction<_MciSendStringNative, _MciSendStringDart>(
+        'mciSendStringW',
+      );
+  final nativeCommand = command.toNativeUtf16();
+  try {
+    final code = mciSendString(nativeCommand, nullptr, 0, 0);
+    if (code != 0 && throwOnError) {
+      throw StateError(
+        'Windows MCI command failed ($code): ${_mciErrorMessage(winmm, code)}',
+      );
+    }
+  } finally {
+    calloc.free(nativeCommand);
+  }
+}
+
+String _mciErrorMessage(DynamicLibrary winmm, int code) {
+  final mciGetErrorString = winmm
+      .lookupFunction<_MciGetErrorStringNative, _MciGetErrorStringDart>(
+        'mciGetErrorStringW',
+      );
+  final buffer = calloc<Uint16>(256).cast<Utf16>();
+  try {
+    final ok = mciGetErrorString(code, buffer, 256);
+    return ok == 0 ? 'Unknown MCI error' : buffer.toDartString();
+  } finally {
+    calloc.free(buffer);
+  }
+}
+
+String _escapeMciPath(String path) {
+  return path.replaceAll('"', '');
 }
--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,6 @@
 import FlutterMacOS
 import Foundation
 
-import audioplayers_darwin
 import file_selector_macos
 import flutter_secure_storage_darwin
 import mobile_scanner
@@ -16,7 +15,6 @@ import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  AudioplayersDarwinPlugin.register(with: registry.registrar(forPlugin: "AudioplayersDarwinPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FlutterSecureStorageDarwinPlugin.register(with: registry.registrar(forPlugin: "FlutterSecureStorageDarwinPlugin"))
   MobileScannerPlugin.register(with: registry.registrar(forPlugin: "MobileScannerPlugin"))
